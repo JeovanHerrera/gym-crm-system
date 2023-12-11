8fe3015fc8cb6a84e7e643e4bf6cd875b746d859
Jeovan Herrera <jeovanis.herrera@jala.university>
2023-12-11 08:48:43 -0500

    Created spring application

---
+++ src/test/java/com/jeovan/gymcrmsystem/GymCrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeovan.gymcrmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCrmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
