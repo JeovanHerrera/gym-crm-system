eb19c974431e6d5003b61265a5d0434b828d2a0b
Jeovan Herrera <jeovanis.herrera@jala.university>
2024-01-23 07:15:54 -0500

    added delete queries for username

--- src/main/java/com/jeovan/gymcrmsystem/daos/TraineeDao.java
+++ src/main/java/com/jeovan/gymcrmsystem/daos/TraineeDao.java
@@ -2,8 +2,10 @@ package com.jeovan.gymcrmsystem.daos;
 
 import com.jeovan.gymcrmsystem.models.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +15,8 @@ public interface TraineeDao extends JpaRepository<Trainee, UUID> {
 
     @Query("SELECT t FROM Trainee t JOIN t.user u WHERE u.username = ?1")
     Optional<Trainee> findByUsername(String username);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Trainee t WHERE EXISTS (SELECT 1 FROM User u WHERE u.username = ?1 AND u = t.user)")
+    int deleteByUsername(String username);
 }
--- src/main/java/com/jeovan/gymcrmsystem/daos/TrainerDao.java
+++ src/main/java/com/jeovan/gymcrmsystem/daos/TrainerDao.java
@@ -2,8 +2,10 @@ package com.jeovan.gymcrmsystem.daos;
 
 import com.jeovan.gymcrmsystem.models.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,4 +14,9 @@ import java.util.UUID;
 public interface TrainerDao extends JpaRepository<Trainer, UUID> {
     @Query("SELECT t FROM Trainer t JOIN t.user u WHERE u.username = ?1")
     Optional<Trainer> findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Trainer t WHERE EXISTS (SELECT 1 FROM User u WHERE u.username = ?1 AND u = t.user)")
+    int deleteByUsername(String username);
 }
